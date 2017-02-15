37d57eb94f021347ded76e147b8d399781ecd66c
Oren Bissick <obissick@greathealthworks.com>
2017-02-15 16:02:23 -0500

    change look and feel to system.

---
+++ src/jmaxscale/JMaxScale.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmaxscale;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author obissick
+ */
+public class JMaxScale {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnsupportedLookAndFeelException{
+        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                double width = screenSize.getWidth();
+                double height = screenSize.getHeight();
+                JFrame frame = new JFrame("MaxScale Admin");
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                //frame.setResizable(false);
+
+                frame.getContentPane().add(new ui());
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
